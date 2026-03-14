e4eabb46337c852e2b135bed97f702e8774420b3
candejt <139970803+candejt@users.noreply.github.com>
2026-03-14 19:59:03 +0100

    Add operate function

--- calculator.js
+++ calculator.js
@@ -1,23 +1,31 @@
-/* function add (a,b){
+const add = function(a,b){
     return a+b
 }
 
-function subtract (a,b){
+const subtract = function (a,b){
     return a-b
 }
 
-function multiply (a,b){
+const multiply = function (a,b){
     return a*b
 }
 
-function divide (a,b) {
+const divide = function (a,b) {
     return a/b    
 }
 
-const operator
-const firstNumber
-const secondNumber
+let operator=''
+let firstNumber=''
+let secondNumber=''
 
-function operate (){
+function operate (op, a, b){
+    if(op==='+'){
+        return add (a,b)
+    }else if (op ==='-') {
+        return subtract (a,b)
+    }else if (op === '*'){
+        return multiply(a,b)
+    }else if (op=== '/'){
+        return divide(a,b)
+    }
 }
-*/
